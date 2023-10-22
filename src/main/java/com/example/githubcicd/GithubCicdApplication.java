0657404139fe8cd0be72f6f246cb5927bda3b4e9
huytvdev <huy8895@gmail.com>
2023-10-22 18:56:25 +0700

    add lombok, test self-hosted.yml

--- src/main/java/com/example/githubcicd/GithubCicdApplication.java
+++ src/main/java/com/example/githubcicd/GithubCicdApplication.java
@@ -1,16 +1,19 @@
 package com.example.githubcicd;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @SpringBootApplication
 @RestController
 public class GithubCicdApplication {
 
     @GetMapping("/welcome")
     public String welcome(){
+        log.info("GithubCicdApplication welcome() called");
         return "welcome!";
     }
 
