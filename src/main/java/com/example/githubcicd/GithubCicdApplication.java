63e8d66aec4027a7805139823725ced413381262
huytvdev <huy8895@gmail.com>
2023-11-21 22:58:05 +0700

    sử dụng github package

--- src/main/java/com/example/githubcicd/GithubCicdApplication.java
+++ src/main/java/com/example/githubcicd/GithubCicdApplication.java
@@ -1,6 +1,8 @@
 package com.example.githubcicd;
 
 import lombok.extern.slf4j.Slf4j;
+import org.example.TestPackage;
+import org.example.TestPackageV2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +20,8 @@ public class GithubCicdApplication {
     }
 
     public static void main(String[] args) {
+        TestPackage.print();
+        TestPackageV2.print();
         SpringApplication.run(GithubCicdApplication.class, args);
     }
 
