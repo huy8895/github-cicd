899bcbe8e1e9b76109defb93a99917028eefa7b2
huytvdev <huy8895@gmail.com>
2023-12-09 23:10:01 +0700

    update config

--- src/main/java/com/example/githubcicd/GithubCicdApplication.java
+++ src/main/java/com/example/githubcicd/GithubCicdApplication.java
@@ -3,6 +3,7 @@ package com.example.githubcicd;
 import lombok.extern.slf4j.Slf4j;
 import org.example.TestPackage;
 import org.example.TestPackageV2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,11 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class GithubCicdApplication {
+    @Value("${spring.application.name}")
+    private String applicationName;
 
     @GetMapping()
     public String welcome(){
         log.info("GithubCicdApplication welcome() called");
-        return "welcome!";
+        return "welcome! to "+ applicationName;
     }
 
     public static void main(String[] args) {
